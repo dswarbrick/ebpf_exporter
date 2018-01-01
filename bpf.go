f2132480525f39d2ab9f2879f271504bf1904d5d
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2018-01-01 18:48:07 +0100

    Add request size histograms to BPF code

--- bpf.go
+++ bpf.go
@@ -13,6 +13,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// NOTE: This BPF program currently causes gobpf's Module.load() function to
+// fail with an ENOSPC error, due to a too small logbuf slice. See
+// https://github.com/iovisor/gobpf/issues/103
+
 package main
 
 const bpfSource string = `
@@ -25,9 +29,16 @@ typedef struct disk_key {
 	u64 slot;
 } disk_key_t;
 
+// Hash to temporily hold the start time of each bio request
 BPF_HASH(start, struct request *);
+
+// Histograms to separately record latencies of read / write requests
 BPF_HISTOGRAM(read_lat, disk_key_t, 32);
 BPF_HISTOGRAM(write_lat, disk_key_t, 32);
+
+// Histograms to separately record sizes of read / write requests
+BPF_HISTOGRAM(read_req_sz, disk_key_t, 16);
+BPF_HISTOGRAM(write_req_sz, disk_key_t, 16);
 
 // Record start time of a request
 int trace_req_start(struct pt_regs *ctx, struct request *req)
@@ -38,7 +49,7 @@ int trace_req_start(struct pt_regs *ctx, struct request *req)
 }
 
 // Calculate request duration and store in appropriate histogram bucket
-int trace_req_completion(struct pt_regs *ctx, struct request *req)
+int trace_req_completion(struct pt_regs *ctx, struct request *req, unsigned int bytes)
 {
 	u64 *tsp, delta;
 
@@ -52,14 +63,20 @@ int trace_req_completion(struct pt_regs *ctx, struct request *req)
 	// Convert to microseconds
 	delta /= 1000;
 
-	// Store as histogram
-	disk_key_t key = {.slot = bpf_log2l(delta)};
-	bpf_probe_read(&key.disk, sizeof(key.disk), req->rq_disk->disk_name);
+	// Latency histogram key
+	disk_key_t lat_key = {.slot = bpf_log2l(delta)};
+	bpf_probe_read(&lat_key.disk, sizeof(lat_key.disk), req->rq_disk->disk_name);
+
+	// Request size histogram key
+	disk_key_t req_sz_key = {.slot = bpf_log2(bytes / 1024)};
+	bpf_probe_read(&req_sz_key.disk, sizeof(req_sz_key.disk), req->rq_disk->disk_name);
 
 	if ((req->cmd_flags & REQ_OP_MASK) == REQ_OP_WRITE) {
-		write_lat.increment(key);
+		write_lat.increment(lat_key);
+		write_req_sz.increment(req_sz_key);
 	} else {
-		read_lat.increment(key);
+		read_lat.increment(lat_key);
+		read_req_sz.increment(req_sz_key);
 	}
 
 	start.delete(&req);
